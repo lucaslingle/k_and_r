2714005142d8e99e309e52a2becdbdb87c01c411
lucaslingle <lucasdaxlingle@gmail.com>
2026-05-07 18:51:38 -0700

    7.1

--- chapter_7/exercise_7.1.c
+++ chapter_7/exercise_7.1.c
@@ -11,9 +11,9 @@ depending on the name it is invoked with, as found in argv[0].
 int main(int argc, char *argv[]) {
     int c;
     while ((c = getchar()) != EOF)
-        if (strncmp(argv[0], "./upper", 5) == 0)
+        if (strncmp(argv[0], "./upper", 7) == 0)
             putchar(toupper(c));
-        else if (strncmp(argv[0], "./lower", 5) == 0)
+        else if (strncmp(argv[0], "./lower", 7) == 0)
             putchar(tolower(c));
         else {
             printf("Error, unexpected invokation name.\n");
